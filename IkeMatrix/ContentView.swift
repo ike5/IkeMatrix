e5c8f3a33ba793c811a8f349283bd0d2c99df331
Ike Maldonado <maldonadoike@gmail.com>
2025-07-18 09:50:29 -0700

    Initial Commit

---
+++ IkeMatrix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IkeMatrix
+//
+//  Created by Ike Maldonado on 7/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IkeMatrix/IkeMatrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  IkeMatrixApp.swift
+//  IkeMatrix
+//
+//  Created by Ike Maldonado on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct IkeMatrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
